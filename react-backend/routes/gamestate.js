0d773d2cd01918c26555d799d5c206d3698d09ff
Andrew Glyadchenko <andrewgly95@gmail.com>
2018-06-20 16:03:23 -0400

    Receive vote in POST request

--- react-backend/routes/gamestate.js
+++ react-backend/routes/gamestate.js
@@ -1,17 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var fs = require("fs");
-var gamestate = require('../database/gamestate.json')
 
 /* GET The State of the Game */
 router.get('/', function(req, res, next) {
   console.log("\n *START* \n");
-  var newState = gamestate;
-  console.log("JSON ", newState);
 
   var content = fs.readFileSync("./database/gamestate.json");
   console.log("Output Content : \n"+ content);
 
+  res.json(JSON.parse(content));
+});
+
+router.post('/', function(req, res, next) {
+  console.log("\n *Received* \n");
+  var vote = req.body.vote;
+  var content = fs.readFileSync("./database/gamestate.json");
+  var oldState = JSON.parse(content);
+  console.log(oldState);
   res.json(JSON.parse(content));
 });
 
