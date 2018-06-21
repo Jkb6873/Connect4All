9ba8acc7500ef4dc9ab77f52f22feb03da925b36
asdacq <hhuang0815@gmail.com>
2018-06-21 11:28:11 -0400

    gamestate.json updates with user click

--- react-backend/routes/gamestate.js
+++ react-backend/routes/gamestate.js
@@ -23,12 +23,43 @@ router.post('/', function(req, res, next) {
   //Reads old state
   var content = fs.readFileSync("./database/gamestate.json");
   var oldState = JSON.parse(content);
+  //var outputState = JSON.stringify(oldState, null, 2);
 
-  console.log(oldState);
 
+  console.log("OUTPUT OLD STATE " + outputState);
+
+  var rowIndex = 5;
+  var columnIndex = vote;
+
+  console.log("VOTE INDEX " + vote);
+  console.log("CHECKING OLDSTATE MEMBER " + oldState.grid[rowIndex][vote]);
+
+  // LOGIC TO UPDATE JSON
+  var flipped = false;
+  
+  while(oldState.grid[rowIndex][vote] != "0"){
+    //console.log("FLIPPED " +flipped);
+    if(oldState.grid[rowIndex][vote] == "0"){
+      flipped = true;
+    }
+    else{
+      rowIndex--;
+    }
+    //oldState.grid[rowIndex][vote] = 1;
+  }
+
+  console.log("Row index: " + rowIndex);
+
+
+  // 1 is a placeholder. It's suppose to be the turn of the player
+  oldState.grid[rowIndex][vote] = 1;
   //
   // FIGURE OUT HOW TO UPDATE JSON
   //
+  var outputState = JSON.stringify(oldState, null, 2);
+
+  console.log("\n NEW \n" + outputState);
+  fs.writeFileSync("./database/gamestate.json", outputState);
 
   res.json(JSON.parse(content));
 });
